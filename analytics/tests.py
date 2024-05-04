ec20ccd58c02702d05191e4e994be516b99c09a1
Akansha <akansharma31@gmail.com>
2024-05-04 20:38:07 +0530

    Add test cases for analytics app

--- analytics/tests.py
+++ analytics/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Vendor
+from rest_framework import status
+from rest_framework.test import APIClient
 
 # Create your tests here.
+
+class TestVendorPerformanceViewse(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.vendor = Vendor.objects.create(name='Test Vendor')
+        return super().setUp()
+    
+    def test_get_all_vendors(self):
+        response = self.client.get(f'/api/vendors/{self.vendor.id}/performance/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- analytics/urls.py
+++ analytics/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import VendorPerformanceViewset
 
 urlpatterns = [
-    path('vendors/<int:pk>/performance',
+    path('vendors/<int:pk>/performance/',
          VendorPerformanceViewset.as_view({'get': 'reterive'}), name='vendor-performance')
 ]
